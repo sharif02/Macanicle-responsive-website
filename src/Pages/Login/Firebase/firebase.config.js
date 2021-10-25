e6d9fe28d804ec36fba47e9d22aa4605cccdd6fc
Sharif <sharifmahmud130@gmail.com>
2021-10-25 21:25:45 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyBlRjq1g8gaU-1cRkyIvy9hvPkC3WtJqXU",
+    authDomain: "machanicale-website.firebaseapp.com",
+    projectId: "machanicale-website",
+    storageBucket: "machanicale-website.appspot.com",
+    messagingSenderId: "156076444239",
+    appId: "1:156076444239:web:705fcbbd64b38cfd8a783a"
+};
+
+export default firebaseConfig;
